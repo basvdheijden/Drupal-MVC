511a40b6812dd96c03b75ee935bfc5408b9d6ebb
andreaswanaar <mail@andreaswarnaar.nl>
2011-11-07 18:35:58 +0100

    Add POC collection and View class.

--- klacht/templates/complaint_overview.tpl.php
+++ klacht/templates/complaint_overview.tpl.php
@@ -1,4 +1,9 @@
 Ik ben gethemed.. yay!!!!
-<h3>
-<?php print $view->name; ?>
-</h3>
+<?php echo count($view['view']->content)?>
+
+<?php var_dump($view['view']->content);?>
+<?php foreach ($view['view']->content as $k => $row ): ?>
+  <br />
+  <strong><?php var_dump($row)?></strong>
+<?  endforeach;?>
+  
